b1223d91ab6b1baabe3b27b0b0b4a492852fe1d1
haochengz <haochengzhao@outlook.com>
2018-06-30 02:51:12 +0800

    doc, style: navigation bar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
+    <img src="./assets/logo.png">
   </div>
 </template>
 
@@ -16,8 +16,9 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+body {
+  margin: 0px;
 }
 </style>
---
+++ frontend/src/components/Nav.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="nav_bar">
+    <div class="logo">
+      <a href="/"><img class="logo-img" src="../assets/logo.png" alt="logo"></a>
+    </div>
+    <div class="navigator">
+      <ul class="list list1">
+        <li class="item item1"><a class="item1 right-item"href="#">Login</a></li>
+        <li class="item item1"> | </li>
+        <li class="item item1">Services</li>
+        <li class="item item1"> | </li>
+        <li class="item item1">Blog</li>
+        <li class="item item1"> | </li>
+        <li class="item item1">CV</li>
+        <li class="item item1"> | </li>
+        <li class="item item1">Portfolio</li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import '../assets/css/nav.css';
+</style>
